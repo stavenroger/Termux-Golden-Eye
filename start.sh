af65db5165051b155751e0bf51333bfd0a7e4065
tadasny <44547407+tadasny@users.noreply.github.com>
2018-10-29 18:00:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 apt update && apt upgrade
-apt install git 
+apt install git
+cd..
 git clone https://github.com/jseidl/GoldenEy
-clear
-apt install python
-clear
 apt install python2
 clear
 echo Please restart Termux!
